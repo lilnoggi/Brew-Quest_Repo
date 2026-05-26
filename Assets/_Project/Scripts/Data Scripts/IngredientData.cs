d3f9ba30e16fa9ae3f41d15f438571d6d1cfd1c5
Mani <itsamanihd@gmail.com>
2026-05-26 23:10:19 +0100

    Ingredients Data
    Added MVP ingredient scriptable objects.

---
+++ Assets/_Project/Scripts/Data Scripts/IngredientData.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+
+// public enum IngredientTier { Poor, Common, Uncommon, Rare, Epic, Mythic, Legendary }
+public enum IngredientTier { Poor, Common, Rare, Mythic } // The quality tier of the ingredient, which will act as a multiplier for the final drink value.
+public enum FlavourProfile { Sweet, Bitter, Fiery, Arcane } // The primary flavour, used to calculate synergy bonuses or clashing penalties when mixed.
+
+[CreateAssetMenu(fileName = "NewIngredient", menuName = "Brew Quest/Ingredient")]
+public class IngredientData : ScriptableObject
+{
+    [Header("Basic Information")]
+    [Tooltip("The display name of the ingredient in the UI.")]
+    [SerializeField] private string _ingredientName;
+
+    [Tooltip("The visual icon representing the ingredient.")]
+    [SerializeField] private Sprite _icon;
+    
+    [Header("Economic Statistics")]
+    [Tooltip("Determines the base quality multiplier.")]
+    [SerializeField] private IngredientTier _tier;
+
+    [Tooltip("Used to check if ingredients mix well together.")]
+    [SerializeField] private FlavourProfile _flavourProfile;
+
+    [Tooltip("The flat gold value this ingredient adds to the baseline price of a custom brew.")]
+    [SerializeField] private int _baseValueContribution; 
+
+    // ===================================================================================================
+
+    // --- Public Read-Only Properties ---
+    // These allow other scripts to read the data without accidentally modifying the master template.
+    public string IngredientName => _ingredientName;
+    public IngredientTier Tier => _tier;
+    public FlavourProfile FlavourProfile => _flavourProfile;
+    public int BaseValueContribution => _baseValueContribution;
+    public Sprite Icon => _icon;
+}
